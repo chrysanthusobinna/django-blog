842e5ec054cf0fac74853e4ba0efd30138db6289
Chrysanthus Chiagwah <chrysanthusobinna@gmail.com>
2025-01-08 18:05:45 +0000

    Add Meta class for post ordering and __str__ method for better post display

--- blog/models.py
+++ blog/models.py
@@ -13,7 +13,12 @@ class Post(models.Model):
     content = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
+    
+    def __str__(self):
+        return f"{self.title} | written by {self.author}"
 
+    class Meta:
+        ordering = ["created_on"]
 
 class Comment(models.Model):
     post = models.ForeignKey(
